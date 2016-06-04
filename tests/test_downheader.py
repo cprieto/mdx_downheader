e9af3c4dd27286c8d862006cba24d109e0298d06
Tobias Bengfort <tobias.bengfort@gmx.net>
2016-06-04 15:04:06 +0200

    write tests

--- tests/test_downheader.py
+++ tests/test_downheader.py
@@ -29,6 +29,45 @@ class TestDownHeaderExtension(unittest.TestCase):
             result = markdown(text, ['downheader',])
             self.assertEqual(expected, result)
 
+    def test_it_downgrade_several_levels(self):
+        """With the extension, we can downgrade the headers several levels"""
+        data = [('# Header 1', '<h3>Header 1</h3>'),
+                ('## Header 2', '<h4>Header 2</h4>'),
+                ('### Header 3', '<h5>Header 3</h5>'),
+                ('#### Header 4', '<h6>Header 4</h6>')]
+        for text, expected in data:
+            result = markdown(text, ['downheader(levels=2)',])
+            self.assertEqual(expected, result)
+
+    def test_it_downgrade_negative_levels(self):
+        """With the extension, we can upgrade the headers"""
+        data = [('## Header 2', '<h1>Header 2</h1>'),
+                ('### Header 3', '<h2>Header 3</h2>'),
+                ('#### Header 4', '<h3>Header 4</h3>'),
+                ('##### Header 5', '<h4>Header 5</h4>'),
+                ('###### Header 6', '<h5>Header 6</h5>')]
+        for text, expected in data:
+            result = markdown(text, ['downheader(levels=-1)',])
+            self.assertEqual(expected, result)
+
+    def test_it_downgrade_clip_at_max(self):
+        """With the extension, output headings have maximum level 6"""
+        data = [('#### Header 4', '<h6>Header 4</h6>'),
+                ('##### Header 5', '<h6>Header 5</h6>'),
+                ('###### Header 6', '<h6>Header 6</h6>')]
+        for text, expected in data:
+            result = markdown(text, ['downheader(levels=2)',])
+            self.assertEqual(expected, result)
+
+    def test_it_downgrade_clip_at_min(self):
+        """With the extension, output headings have minimum level 1"""
+        data = [('# Header 1', '<h1>Header 1</h1>'),
+                ('## Header 2', '<h1>Header 2</h1>'),
+                ('### Header 3', '<h1>Header 3</h1>')]
+        for text, expected in data:
+            result = markdown(text, ['downheader(levels=-2)',])
+            self.assertEqual(expected, result)
+
     def test_if_it_only_register_extension_once(self):
         text_input = "# Header 1"
         expected = "<h2>Header 1</h2>"
